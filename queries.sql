fe50cc86cd4fd329420664a1987417f9f1cd4548
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-11-04 17:05:35 -0600

    completed

--- queries.sql
+++ queries.sql
@@ -1,9 +1,46 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select
+    p.productname as 'Product Name',
+    c.categoryname as 'Category'
+from Product as p
+join Category as c
+    on p.categoryid = c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+select
+    o.id as 'order id',
+    s.companyname as 'Company',
+    o.OrderDate as 'order date'
+from "Order" as o
+join "Shipper" as s
+    on o.shipvia = s.id
+where o.OrderDate < '2012-08-09'
+order by OrderDate;
+
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+
+
+Select 
+    p.ProductName as 'product name', 
+    od.Quantity as 'quantity'
+from Product as p
+join OrderDetail as od
+    on od.ProductId = p.Id
+where od.OrderId = '10251';
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+Select 
+    o.Id as 'order Id',
+    c.CompanyName as 'Company Name', 
+    e.LastName as 'Employee Last Name'
+from "Order" as o
+join Customer as c
+    on o.CustomerId = c.Id
+join Employee as e
+     on o.EmployeeId = e.Id;
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,67 @@
 // scheme-model
+const db = require("../data/db-config.js");
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find(){
+    return db('schemes')
+}
+
+function findById(id){
+    return db('schemes')
+    .where({id})
+    .first()
+}
+
+function findSteps(id){
+    return db('schemes')
+    .join('steps','steps.scheme_id', 'schemes.id')
+    .select(
+        'step.id',
+        'schemes.scheme_name',
+        'steps.step_number',
+        'steps.instructions'
+    )
+    .where({'schemes:id':id})
+    .orderBy('steps.step_number')
+}
+
+
+function add(scheme){
+    return db('schemes')
+    .insert(scheme)
+    .then((id) => {
+        return db('schemes')
+    .where({ id: id }).first()
+    })
+    .catch((err) => {
+        return null
+    })
+}
+
+function update (update,id){
+    db("scheme")
+    .where({ id })
+    .update(changes)
+    .then((scheme) => {
+      if (scheme) {
+        return findById(id);
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      return null;
+    });
+  }
+
+function remove (id){
+    return db("schemes").where({ id }).del();   
+}
